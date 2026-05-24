ee5d2eb52795f5265536c7d796dee8e810aab58e
JohananJuan <168384417+JohananJuan@users.noreply.github.com>
2026-05-23 18:48:50 -0600

    Add sum calculation and output to Program.cs
    Added a variable to calculate the sum of two numbers and output the result.

--- Codigo/Git/Ejercicio_GIT/Ejercicio_GIT/Program.cs
+++ Codigo/Git/Ejercicio_GIT/Ejercicio_GIT/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int suma= 3 + 2;
+            Console.WriteLine(suma);
         }
     }
 }
