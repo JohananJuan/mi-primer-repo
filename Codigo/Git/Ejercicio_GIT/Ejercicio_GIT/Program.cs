61e277ceb921727bfc7db7bcd651298bc724560e
JohananJuan <168384417+JohananJuan@users.noreply.github.com>
2026-05-23 18:53:48 -0600

    agrega metodo resta

--- Codigo/Git/Ejercicio_GIT/Ejercicio_GIT/Program.cs
+++ Codigo/Git/Ejercicio_GIT/Ejercicio_GIT/Program.cs
@@ -4,8 +4,9 @@
     {
         static void Main(string[] args)
         {
-            int suma= 3 + 2;
-            Console.WriteLine(suma);
+            int suma = 3 + 2;
+            int resta = 3 + 2; 
+            Console.WriteLine(suma / resta);
         }
     }
 }
