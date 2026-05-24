c70aa1631bad43bd04236d961d244ca77f54c7d1
JohananJuan <168384417+JohananJuan@users.noreply.github.com>
2026-05-24 17:58:29 -0600

    chore: agrega ejercicios de la seccion 14 del curso Aprendiendo a Progamar en c#

---
+++ Codigo/Git/Seccion14_380/Seccion14_380/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Seccion14_380
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.WriteLine("Texto de prueba");
+
+            //Console.ForegroundColor = ConsoleColor.DarkGreen;
+            //Console.WriteLine("Texto de prueba 2");
+
+            //Console.ResetColor ();
+            //Console.WriteLine("Texto de prueba 2");
+
+
+            // Resaltar texto
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.OutputEncoding = Encoding .UTF8;
+            Console.WriteLine("👌 Texto de prueba");
+            Console.WriteLine(" .----------------.  .----------------.  .----------------.  .----------------. \r\n| .--------------. || .--------------. || .--------------. || .--------------. |\r\n| |  ____  ____  | || |     ____     | || |   _____      | || |      __      | |\r\n| | |_   ||   _| | || |   .'    `.   | || |  |_   _|     | || |     /  \\     | |\r\n| |   | |__| |   | || |  /  .--.  \\  | || |    | |       | || |    / /\\ \\    | |\r\n| |   |  __  |   | || |  | |    | |  | || |    | |   _   | || |   / ____ \\   | |\r\n| |  _| |  | |_  | || |  \\  `--'  /  | || |   _| |__/ |  | || | _/ /    \\ \\_ | |\r\n| | |____||____| | || |   `.____.'   | || |  |________|  | || ||____|  |____|| |\r\n| |              | || |              | || |              | || |              | |\r\n| '--------------' || '--------------' || '--------------' || '--------------' |\r\n '----------------'  '----------------'  '----------------'  '----------------' ");
+            Console.WriteLine("                     .   .  .       .  .   ...               .      \r\n     .        . .    ..      .       .      ..            .  . .    \r\n    .  .    . .        .  ..  . .     ..                         .  \r\n       .        .   . .          .       .                      .   \r\n. .      .    .          .                     .  ...    .          \r\n.         .           ......... .             .         .. .  .     \r\n   .   .          ..........::.. . ..    .                      .   \r\n.                .........:::::    .   .    .                  .. ..\r\n .     .... .   ...:::...:::::::.               ..  .      .   .    \r\n      ....:::.  . .::..:::+@@@@@@-.-..*@%#*-.      .        ..  ..  \r\n . . .....::::.   .:::::::::*@@@@@@@@@@@%#:  . .                .   \r\n  .    ....::......::::::::::-=+#@@@@+:. ....     . .   ..          \r\n  .    . .......:.... .....:::-::--*----:....                     . \r\n     .  ..... .....::.:.::::::::::::::-----:  .           ....      \r\n        .....    ..::::.....:::::...:::::::                         \r\n.     .   ....   ..:::...............:::::.   .  . ..  .  . . ..    \r\n   .            ............................            . .         \r\n    .      .    .........................          .  ..            \r\n        . .      ...............       .               .            \r\n  .. .    .        ..  ..  . ..    .     .       .  .  . .      .   \r\n .  .      .        ..       .         .             .              \r\n                           .. . .       .   . .      .      .       \r\n             .      .      .    .                 .        .  ..    ");
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("╔═════════╗");
+            Console.WriteLine("║         ║");
+            Console.WriteLine("║         ║");
+            Console.WriteLine("║         ║");
+            Console.WriteLine("╚═════════╝");
+        }
+    }
+}
---
+++ Codigo/Git/Seccion14_385/Seccion14_385/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Seccion14_385
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ciclo for de 0 al 100 y de 10 en 10
+            for (int i = 0; i <= 100;  i +=10)
+            {
+                Console.Clear();
+
+                // Marca el inicio de la barra
+                Console.Write("\n[");
+
+                // Simulacion de llenado
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                int barrasLlenas = i / 5;
+
+                Console.Write(new string('▌',barrasLlenas));
+
+                // Despes de dibujar los caracteres de la tarea vacia 
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                int barrasVacias = 20 - barrasLlenas;
+                Console.Write(new string('▌',barrasVacias));
+
+                Console.ResetColor();
+
+                // Marca el final de la tabla y nos muestra el porcentaje
+                Console.Write($"] {i}%\n");
+
+                Thread.Sleep(300);
+
+            }
+        }
+    }
+}
